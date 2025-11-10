677c69ba00b095a53a1a036deaf045e50f23cd85
Lagui117 <guillaumewatelet06@gmail.com>
2025-11-10 15:55:07 +0100

    partie 3

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restx import Api
-from app.persistence.repository import SQLAlchemyRepository  # Import the new repository class
-from app.extensions import db, bcrypt, jwt  # Use extensions for database and authentication
+from app.persistence.repository import SQLAlchemyRepository  
+from app.extensions import db, bcrypt, jwt 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
